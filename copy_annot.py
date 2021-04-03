9bc29f247e4ed2ea094abb00c69cc6313260186f
ubuntu <compspandan@gmail.com>
2021-04-03 15:54:21 +0530

    added new file to copy annotation using text search added scaling factor to correct glitch

--- copy_annot.py
+++ copy_annot.py
@@ -1,31 +1,29 @@
 import fitz
-red = (1, 0, 0)
-def print_descr(annot):
-    """Print a short description to the right of each annot rect."""
-    annot.parent.insert_text(
-        annot.rect.br + (10, -5), "%s annotation" % annot.type[1], color=red
-    )
 
 def main():
-    src = fitz.open("input.pdf")
-    dest = fitz.open("output.pdf")
-    pagesrc = src[0]
-    pagedes = dest[0]
-    annot = pagesrc.firstAnnot
-
-    while annot:
-        if annot.type[0] in (8, 9, 10, 11): # one of the 4 types above
-            print(len(annot.vertices))
-            i=0
-            j=4
-            while j<=len(annot.vertices):
-                rect = fitz.Quad(annot.vertices[i:j])
-                print(rect)
-                pagedes.addHighlightAnnot(rect)
-                i=i+4
-                j=j+4
-        annot=annot.next
-    #dest.saveIncr() #to save in original file
-    dest.save("output1.pdf") #to save in new file
+    src = fitz.open("KuroseCh1-2.pdf")
+    dest = fitz.open("James-Kurose-Keith-Ross-Computer-Networking_-A-Top-Down-Approach-7th-Edition.pdf")
+    for i in range(176,177):#pages start from 0
+        pagesrc = src[i]
+        pagedes = dest[i]
+        # print(pagesrc.rect)
+        # print(pagedes.rect)
+        scaleX=pagedes.rect[2]/pagesrc.rect[2]
+        scaleY=pagedes.rect[3]/pagesrc.rect[3]
+        m = fitz.Matrix(scaleX,0,0,scaleY,0,0)
+        annot = pagesrc.firstAnnot
+        while annot:
+            if annot.type[0] in (8, 9, 10, 11): # one of the 4 types above
+                i=0
+                j=4
+                while j<=len(annot.vertices):
+                    rect = fitz.Quad(annot.vertices[i:j])
+                    rect.transform(m)
+                    pagedes.addHighlightAnnot(rect)
+                    i=i+4
+                    j=j+4
+            annot=annot.next
+        #dest.saveIncr() #to save in original file
+        dest.save("output1.pdf") #to save in new file
 if __name__=="__main__":
     main()
---
+++ copy_annot_with_search.py
@@ -0,0 +1,45 @@
+import fitz
+
+def make_text(words):
+    """Return textstring output of getText("words").
+    Word items are sorted for reading sequence left to right,
+    top to bottom.
+    """
+    line_dict = {}  # key: vertical coordinate, value: list of words
+    words.sort(key=lambda w: w[0])  # sort by horizontal coordinate
+    for w in words:  # fill the line dictionary
+        y1 = round(w[3], 1)  # bottom of a word: don't be too picky!
+        word = w[4]  # the text of the word
+        line = line_dict.get(y1, [])  # read current line content
+        line.append(word)  # append new word
+        line_dict[y1] = line  # write back to dict
+    lines = list(line_dict.items())
+    lines.sort()  # sort vertically
+    return "\n".join([" ".join(line[1]) for line in lines])
+
+def main():
+    src = fitz.open("KuroseCh1-2.pdf")
+    dest = fitz.open("James-Kurose-Keith-Ross-Computer-Networking_-A-Top-Down-Approach-7th-Edition.pdf")
+    for i in range(176,177):#pages start from 0
+        pagesrc = src[i]
+        pagedes = dest[i]
+        words = pagesrc.getText("words")
+        annot = pagesrc.firstAnnot
+        while annot:
+            if annot.type[0] in (8, 9, 10, 11): # one of the 4 types above
+                i=0
+                j=4
+                while j<=len(annot.vertices):
+                    rect = fitz.Quad(annot.vertices[i:j]).rect
+                    mywords = [w for w in words if fitz.Rect(w[:4]).intersect(rect)]
+                    search = make_text(mywords)
+                    print(search)
+                    quads = pagedes.search_for(search, quads=True)
+                    pagedes.addHighlightAnnot(quads)
+                    i=i+4
+                    j=j+4
+            annot=annot.next
+        #dest.saveIncr() #to save in original file
+        dest.save("output1.pdf") #to save in new file
+if __name__=="__main__":
+    main()
